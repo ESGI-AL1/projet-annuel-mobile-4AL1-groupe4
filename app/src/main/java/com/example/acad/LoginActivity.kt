f0b6594897a00f678dc2649a1e66e99049d065de
catri-01 <catrimaouene@gmail.com>
2024-06-15 00:43:47 +0200

    creation file login activity

---
+++ app/src/main/java/com/example/acad/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.acad
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.acad.R
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.navigation.NavigationView
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+    }
+}
