54d3a25ddafca0e9dcc61c799a66e9a84ddc7b59
catri-01 <catrimaouene@gmail.com>
2024-06-15 00:49:53 +0200

    The code initializes the activity login and configures its user interface.

--- app/src/main/java/com/example/acad/LoginActivity.kt
+++ app/src/main/java/com/example/acad/LoginActivity.kt
@@ -15,10 +15,23 @@ import com.google.android.material.navigation.NavigationView
 
 class LoginActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnLogin = findViewById<Button>(R.id.loginButton)
+
+        val backBtn: MaterialButton = findViewById(R.id.backButton)
+        backBtn.setOnClickListener {
+            finish()
+        }
 
     }
 }
