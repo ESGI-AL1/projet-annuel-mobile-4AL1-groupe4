bbdb297ce668013f0353e6d1395feae2622c1507
catri-01 <catrimaouene@gmail.com>
2024-06-15 01:10:49 +0200

    add the interface of login & register in mainActivity

--- app/src/main/java/com/example/acad/MainActivity.kt
+++ app/src/main/java/com/example/acad/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.acad
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        val registerButton = findViewById<Button>(R.id.btn_signup)
+
+        loginButton.setOnClickListener {
+            // Handle login button click
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        registerButton.setOnClickListener {
+            // Handle register button click
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
