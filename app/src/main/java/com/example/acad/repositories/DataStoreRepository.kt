64c19ae495d0338a6c4d6f04b0845168aee48a78
catri-01 <catrimaouene@gmail.com>
2024-06-21 14:13:47 +0200

    add the methode for CREAD the token

--- app/src/main/java/com/example/acad/repositories/DataStoreRepository.kt
+++ app/src/main/java/com/example/acad/repositories/DataStoreRepository.kt
@@ -25,5 +25,49 @@ class DataStoreRepository(context: Context) {
     private val accessToken = context.accessToken
     private val refreshToken = context.refreshToken
 
-    
+    suspend fun saveAccessToken(token: String) {
+        accessToken.edit { preferences ->
+            preferences[PreferencesKey.accessToken] = token
+        }
+    }
+
+    suspend fun saveRefreshToken(token: String) {
+        refreshToken.edit { preferences ->
+            preferences[PreferencesKey.refreshToken] = token
+        }
+    }
+
+    fun readAccessToken(): Flow<String> {
+        return accessToken.data.catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            val accessToken = preferences[PreferencesKey.accessToken] ?: ""
+            accessToken
+        }
+    }
+
+    fun readRefreshToken(): Flow<String> {
+        return refreshToken.data.catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            val refreshToken = preferences[PreferencesKey.refreshToken] ?: ""
+            refreshToken
+        }
+    }
+
+    suspend fun removeAccessToken() {
+        accessToken.edit { preferences -> preferences.clear() }
+    }
+
+    suspend fun removeRefreshToken() {
+        refreshToken.edit { preferences -> preferences.clear() }
+    }
 }
