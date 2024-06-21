7334776399a3298caee187f26e30be929dbd4f38
catri-01 <catrimaouene@gmail.com>
2024-06-21 13:59:58 +0200

    add the auth in the repository

--- app/src/main/java/com/example/acad/repositories/AuthRepository.kt
+++ app/src/main/java/com/example/acad/repositories/AuthRepository.kt
@@ -1,8 +1,25 @@
 package com.example.acad.repositories
 
+import com.example.acad.models.Jwt
+import com.example.acad.models.User
+import com.example.acad.requests.LoginRequest
+import com.example.acad.requests.UserRequest
+import com.example.acad.services.AuthService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(
-    private val service:
+    private val service: AuthService
 ) {
+
+    fun registerUser(request: UserRequest): Flow<User> = flow {
+        emit(service.register(request))
+    }.flowOn(Dispatchers.IO)
+
+    fun loginUser(request: LoginRequest): Flow<Jwt> = flow {
+        emit(service.login(request))
+    }
 }
