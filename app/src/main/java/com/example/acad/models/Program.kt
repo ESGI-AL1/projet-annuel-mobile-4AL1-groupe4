0a5db1037f52314806c9cc35a30aa22fa4126440
catri-01 <catrimaouene@gmail.com>
2024-07-08 20:29:53 +0200

    add the constructor creator program

--- app/src/main/java/com/example/acad/models/Program.kt
+++ app/src/main/java/com/example/acad/models/Program.kt
@@ -2,39 +2,47 @@ package com.example.acad.models
 
 import android.os.Parcel
 import android.os.Parcelable
-import java.util.ArrayList
+import com.google.gson.annotations.SerializedName
+import java.util.Date
 
 data class Program(
     val id: Int,
+    val file: String,
+    @SerializedName("created_at")
+    val createdAt: String,
+    val isVisible: Boolean,
     val title: String,
     val date: String,
     val description: String,
     val tags: ArrayList<String>?,
     val profileImage: Int,
     val code: String,
-    val author: String,
-): Parcelable {
+    val author: Any?,
+) : Parcelable {
     constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.createStringArrayList(),
-        parcel.readInt(),
-        parcel.readString().toString(),
-        parcel.readString().toString()
+        id = parcel.readInt(),
+        file = parcel.readString().toString(),
+        createdAt = parcel.readString().toString(),
+        title = parcel.readString().toString(),
+        tags = parcel.createStringArrayList(),
+        profileImage = parcel.readInt(),
+        code = parcel.readString().toString(),
+        description = parcel.readString().toString(),
+        author = parcel.readValue(User::class.java.classLoader),
+        isVisible = parcel.readByte() != 0.toByte(),
+        date = parcel.readString().toString()
     ) {
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(id)
         parcel.writeString(title)
-        parcel.writeString(date)
+        parcel.writeString(createdAt)
         parcel.writeString(description)
         parcel.writeStringList(tags)
         parcel.writeInt(profileImage)
         parcel.writeString(code)
-        parcel.writeString(author)
+        parcel.writeValue(author)
     }
 
     override fun describeContents(): Int {
@@ -51,3 +59,18 @@ data class Program(
         }
     }
 }
+
+data class ShowProgram(
+    val id: Int,
+    val file: String,
+    @SerializedName("created_at")
+    val createdAt: String,
+    val isVisible: Boolean,
+    val title: String,
+    val date: String,
+    val description: String,
+    val tags: ArrayList<String>?,
+    val profileImage: Int,
+    val code: String,
+    val author: User,
+)
