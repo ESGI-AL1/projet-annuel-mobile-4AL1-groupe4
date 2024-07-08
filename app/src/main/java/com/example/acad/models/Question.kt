b59fcccaa34cf2b837feda296c91ad17b81227c0
catri-01 <catrimaouene@gmail.com>
2024-07-08 20:33:10 +0200

    This function creates and returns an array of specified size (size), where each element is of type Question?

--- app/src/main/java/com/example/acad/models/Question.kt
+++ app/src/main/java/com/example/acad/models/Question.kt
@@ -2,16 +2,23 @@ package com.example.acad.models
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 
 data class Question(
     val profileImage: Int, // Assuming you are using a drawable resource ID
     val authorName: String,
+    @SerializedName("created_at")
     val questionTime: String,
+    @SerializedName("title")
     val questionTitle: String,
+    @SerializedName("description")
     val questionDescription: String,
     val tags: List<String>,
+    @SerializedName("views_count")
     val viewsCount: Int,
+    @SerializedName("comments_count")
     val commentsCount: Int,
+    @SerializedName("votes_count")
     val votesCount: Int
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
