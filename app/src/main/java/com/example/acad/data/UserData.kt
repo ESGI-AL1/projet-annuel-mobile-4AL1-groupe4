5bfe2b0cd944d006e5892d2c560737ccc815aebb
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:20:55 +0200

    add the state userdata

--- app/src/main/java/com/example/acad/data/UserData.kt
+++ app/src/main/java/com/example/acad/data/UserData.kt
@@ -1,5 +1,8 @@
 package com.example.acad.data
 
+import android.content.ContentValues
+import android.util.Log
+import com.example.acad.models.Member
 import com.example.acad.models.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -8,9 +11,32 @@ import javax.inject.Inject
 class UserData @Inject constructor() {
 
     private var _userState: MutableStateFlow<User> = MutableStateFlow(User())
+    private var _list: MutableList<User> = mutableListOf()
     val userState: StateFlow<User> get() = _userState
+    val list: List<User> get() = _list
 
     fun save(user: User) {
         _userState.value = user
     }
+
+    fun saveList(list: List<User>) {
+        _list = list as MutableList<User>
+    }
+
+    fun getUser(index: Long): User? {
+        Log.d(ContentValues.TAG, "getUser: $index")
+        return list.find { user -> user.id.toLong() == index }
+    }
+
+    fun getUser(username: String): User? {
+        return list.find { it.username == username }
+    }
 }
+
+fun User.toMember(): Member = Member(
+    id = id,
+    username = username,
+    name = username,
+    bio = "",
+    imageResId = 0
+)
