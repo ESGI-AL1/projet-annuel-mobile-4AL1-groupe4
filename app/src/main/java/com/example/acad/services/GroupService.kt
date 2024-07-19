7da964905c55bcb709724f02b9a818cc20d0a74b
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:02:17 +0200

    add the request in Group

--- app/src/main/java/com/example/acad/services/GroupService.kt
+++ app/src/main/java/com/example/acad/services/GroupService.kt
@@ -1,9 +1,13 @@
 package com.example.acad.services
 
 import com.example.acad.models.Group
+import com.example.acad.requests.GroupRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface GroupService {
 
@@ -14,4 +18,12 @@ interface GroupService {
     @GET("groups/public/all/")
     @Headers("Content-Type: application/json; charset=utf-8")
     suspend fun publicList(@Header("Authorization") token: String): List<Group>
+
+    @POST("groups/")
+    @Headers("Content-Type: application/json; charset=utf-8")
+    suspend fun create(@Header("Authorization") token: String, @Body request: GroupRequest): Group
+
+    @GET("groups/{id}/")
+    @Headers("Content-Type: application/json; charset=utf-8")
+    suspend fun show(@Header("Authorization") token: String, @Path("id") groupId: String?): Group
 }
