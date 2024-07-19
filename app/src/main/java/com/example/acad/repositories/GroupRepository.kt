95839f9ea6904d6e9f2651051d737c81aa3dcc6a
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:09:55 +0200

    add the function getgroup in group Repository

--- app/src/main/java/com/example/acad/repositories/GroupRepository.kt
+++ app/src/main/java/com/example/acad/repositories/GroupRepository.kt
@@ -1,7 +1,7 @@
 package com.example.acad.repositories
 
 import com.example.acad.models.Group
-import com.example.acad.models.Program
+import com.example.acad.requests.GroupRequest
 import com.example.acad.services.GroupService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class GroupRepository @Inject constructor(private val service: GroupService){
+class GroupRepository @Inject constructor(private val service: GroupService) {
     fun listProgram(token: String): Flow<List<Group>> = flow {
         emit(service.list(token = "Bearer $token"))
     }.flowOn(Dispatchers.IO)
@@ -17,4 +17,12 @@ class GroupRepository @Inject constructor(private val service: GroupService){
     fun publicListGroup(token: String): Flow<List<Group>> = flow {
         emit(service.publicList(token = "Bearer $token"))
     }.flowOn(Dispatchers.IO)
+
+    fun createGroup(token: String, request: GroupRequest): Flow<Group> = flow {
+        emit(service.create(token = "Bearer $token", request))
+    }.flowOn(Dispatchers.IO)
+
+    fun getGroup(token: String, groupId: String?): Flow<Group> = flow {
+        emit(service.show(token = "Bearer $token", groupId))
+    }
 }
