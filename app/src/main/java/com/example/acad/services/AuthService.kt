f43691faaea35ad04c73a49469594529034b9cc6
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:01:44 +0200

    add the request in Auth

--- app/src/main/java/com/example/acad/services/AuthService.kt
+++ app/src/main/java/com/example/acad/services/AuthService.kt
@@ -1,14 +1,15 @@
 package com.example.acad.services
 
-import com.example.acad.models.HttpResponse
-import com.example.acad.models.HttpResponse.SuccessResponse
 import com.example.acad.models.Jwt
 import com.example.acad.models.User
 import com.example.acad.requests.LoginRequest
 import com.example.acad.requests.UserRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface AuthService {
 
@@ -16,7 +17,15 @@ interface AuthService {
     @Headers("Content-Type: application/json; charset=utf-8")
     suspend fun register(@Body request: UserRequest): User
 
-    @POST("user/login/")
+    @POST("token/")
     @Headers("Content-Type: application/json; charset=utf-8")
-    suspend fun login(@Body request: LoginRequest): SuccessResponse<Jwt>
+    suspend fun login(@Body request: LoginRequest): Jwt
+
+    @GET("user/informations/{id}/")
+    @Headers("Content-Type: application/json; charset=utf-8")
+    suspend fun userInfo(@Header("Authorization") token: String, @Path("id") userId: String): User
+
+    @GET("user/all/")
+    @Headers("Content-Type: application/json; charset=utf-8")
+    suspend fun list(@Header("Authorization") token: String): List<User>
 }
