10ebf142bce02259ee380135c34e0ecdca757dd4
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:10:24 +0200

    add the notification file repository

---
+++ app/src/main/java/com/example/acad/repositories/NotificationRepository.kt
@@ -0,0 +1,19 @@
+package com.example.acad.repositories
+
+import com.example.acad.models.Notification
+import com.example.acad.services.NotificationService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NotificationRepository @Inject constructor(private val service: NotificationService) {
+    fun listNotification(token: String): Flow<List<Notification>> = flow {
+        emit(service.list(token = "Bearer $token"))
+    }.flowOn(Dispatchers.IO)
+
+    fun showNotification(token: String, id: String): Flow<Notification> = flow {
+        emit(service.show(token = "Bearer $token", id = id))
+    }.flowOn(Dispatchers.IO)
+}
