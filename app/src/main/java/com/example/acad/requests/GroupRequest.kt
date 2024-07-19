79c899f5fd715488e680a134b1a4e0cb8174b251
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:05:00 +0200

    add the GroupRequest file

---
+++ app/src/main/java/com/example/acad/requests/GroupRequest.kt
@@ -0,0 +1,11 @@
+package com.example.acad.requests
+
+import com.google.gson.annotations.SerializedName
+
+data class GroupRequest(
+    @SerializedName("name")
+    val title: String,
+    val description: String,
+    val members: List<Int?>,
+    val photo: String
+)
