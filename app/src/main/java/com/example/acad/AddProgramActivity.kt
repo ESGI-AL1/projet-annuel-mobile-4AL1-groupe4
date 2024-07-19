1eb9e5949f10498b1fbe1659bc97d5391ab62d04
catri-01 <catrimaouene@gmail.com>
2024-07-19 16:39:44 +0200

    add the injection field

--- app/src/main/java/com/example/acad/AddProgramActivity.kt
+++ app/src/main/java/com/example/acad/AddProgramActivity.kt
@@ -1,7 +1,9 @@
 package com.example.acad
 
+import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
@@ -12,21 +14,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.example.acad.adapters.ProgramAdapter
 import com.example.acad.repositories.DataStoreRepository
 import com.example.acad.repositories.ProgramRepository
 import com.example.acad.requests.LoginRequest
 import com.example.acad.requests.ProgramRequest
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
+import java.io.File
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class AddProgramActivity : AppCompatActivity() {
+    private lateinit var adapter: ProgramAdapter
 
     @Inject
     lateinit var programRepository: ProgramRepository
@@ -37,6 +43,9 @@ class AddProgramActivity : AppCompatActivity() {
     private lateinit var tagsTextEdit: AutoCompleteTextView
     private lateinit var titleTextEdit: EditText
     private lateinit var descriptionTextEdit: EditText
+    private lateinit var fileInput: TextInputEditText
+    private val PICK_FILE_REQUEST = 1
+    private var selectedFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,6 +78,11 @@ class AddProgramActivity : AppCompatActivity() {
         tagsTextEdit = findViewById(R.id.languagesInput)
         titleTextEdit = findViewById(R.id.titleInput)
         descriptionTextEdit = findViewById(R.id.descriptionInput)
+        fileInput = findViewById(R.id.fileInput)
+
+        fileInput.setOnClickListener {
+            openFileChooser()
+        }
 
         createProgramButton.setOnClickListener {
             // Gérer le clic du bouton pour créer le programme
@@ -76,7 +90,8 @@ class AddProgramActivity : AppCompatActivity() {
             val request = ProgramRequest(
                 title = titleTextEdit.text.toString(),
                 description = descriptionTextEdit.text.toString(),
-                tags = tagsTextEdit.text.toString().split(",").map { it.trim() }
+                tags = tagsTextEdit.text.toString().split(",").map { it.trim() },
+                file = selectedFile!!
             )
 
             lifecycleScope.launch {
@@ -97,8 +112,39 @@ class AddProgramActivity : AppCompatActivity() {
                 }
                 .collect { response ->
                     Log.d(TAG, "launchRequest: $response")
+                    programRepository.listProgram(it).collect{ programs ->
+                        adapter.updateData(programs)
+                    }
                     finish()
                 }
         }
+    }
+
+    private fun openFileChooser() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivityForResult(intent, PICK_FILE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_FILE_REQUEST && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+                val fileName = getFileName(uri)
+                fileInput.setText(fileName)
+                selectedFile = uri.path?.let { File(it) }
+            }
+        }
+    }
+
+    private fun getFileName(uri: Uri): String {
+        var fileName = "Unknown"
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                fileName = it.getString(it.getColumnIndexOrThrow("_display_name"))
+            }
+        }
+        return fileName
     }
 }
