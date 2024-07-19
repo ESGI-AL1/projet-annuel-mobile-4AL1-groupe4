392cdccbf7261933250f468266e5c71a60bc3698
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:17:49 +0200

    add the user model

--- app/src/main/java/com/example/acad/models/User.kt
+++ app/src/main/java/com/example/acad/models/User.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class User(
     @SerializedName("id") val id: Int,
     @SerializedName("username") val username: String,
-    @SerializedName("email") val email: String
+    @SerializedName("email") val email: String,
+    val isOnline: Boolean = false
 ) {
     constructor(): this(0, "", "")
 }
