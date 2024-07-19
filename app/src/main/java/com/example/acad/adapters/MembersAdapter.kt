d945e27d33c3687c8dadb0561e96e0651f7d10f3
catri-01 <catrimaouene@gmail.com>
2024-07-19 17:24:07 +0200

    add the members Adapter in adapter

---
+++ app/src/main/java/com/example/acad/adapters/MembersAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.acad.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.acad.R
+import com.example.acad.models.Member
+import com.example.acad.viewholders.MemberViewHolder
+
+class MembersAdapter(private val members: List<Member>) : RecyclerView.Adapter<MemberViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberViewHolder {
+        val binding = LayoutInflater.from(parent.context).inflate(R.layout.item_member, parent, false)
+        return MemberViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MemberViewHolder, position: Int) {
+        val member = members[position]
+        holder.username.text = member.name
+        holder.email.text =member.username
+        holder.description.text = member.bio
+        holder.image.setImageResource(R.drawable.ic_profile)
+    }
+
+    override fun getItemCount(): Int = members.size
+
+}
